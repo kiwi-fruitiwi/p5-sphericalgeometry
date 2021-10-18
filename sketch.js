4e4b477a037e29f5a22716982c73af89f1dec366
kiwi-fruitiwi <zane.tian@gmail.com>
2021-10-18 10:31:17 -0700

    refactor subsections into functions. understand sphere_detail values

--- sketch.js
+++ sketch.js
@@ -14,14 +14,15 @@ coding plan
 .   draw colored axes using beginHUD
 .   movable pyramid with WASD keys
     .   wrap around the sphere when encountering a boundary
-    dynamically vary the sphere detail
+.   dynamically vary the sphere detail
 
 BUGS
-    when both θ and φ go from 0 to 2π, we actually draw the sphere twice
-        this is required if we want easy wrapping for WASD though
+    when both θ and φ go from 0 to 2π, we actually set up two sets of
+    overlapping points for our sphere. this is required if we want easy
+    wrapping for WASD though
 
 TODO
-    make a second globe  with larger r and store coordinates in setup
+    make a second globe with larger r and store coordinates in setup
         toggle to make pyramid jump to second globe coordinates
         try more globes at 0.01 radius increments around r from 50-150
             later sine wave will be amp < 50!
@@ -29,21 +30,21 @@ TODO
  */
 let font
 let cam
-let TOTAL=16
+let SPHERE_DETAIL = 5 // number of segments per θ and φ
 
 // define the hue and saturation for all 3 axes
-const X_HUE=0, X_SAT=80, Y_HUE=90, Y_SAT=80, Z_HUE=210, Z_SAT=80
+const X_HUE = 0, X_SAT = 80, Y_HUE = 90, Y_SAT = 80, Z_HUE = 210, Z_SAT = 80
 const DIM = 40
 const BRIGHT = 75
 
+// an n by n 2D array of points on a sphere in (r, θ, φ) triples
+let globe
 
 
-
-// initialize globe, our 2D array of 3D vectors
-let globe = Array(TOTAL+1)
-for (let i=0; i<TOTAL+1; i++) {
-    globe[i] = Array(TOTAL+1)
-}
+// these keep track of the top left corner of the quad projection to the
+// sphere's surface from the origin. unsure if projection is the correct word
+let projection_x = 0
+let projection_y = 0
 
 
 // prevent the context menu from showing up :3 nya~
@@ -63,36 +64,34 @@ function setup() {
     textFont(font, 16)
 
     cam = new Dw.EasyCam(this._renderer, {distance:240});
-
-    // TODO find out how to place slider properly
-    // total = createSlider(1, 80, 20, 2)
-
 }
-
-
-let telescope = []
-let telescope_index = 1
-let quad_index = 1
-
-// these keep track of the top left corner of the quad projection to the
-// sphere's surface from the origin. unsure if projection is the correct word
-let projection_x = 0
-let projection_y = 0
 
 
 // TODO why does alpha not work in WEBGL 3D
 function draw() {
     background(234, 34, 24)
-    stroke(0, 0, 60, 20)
-    strokeWeight(1)
     lights()
+
     drawBlenderAxes()
+    populateGlobeArray()
+    displayGlobe()
+    drawPyramid()
+    displayHUD()
+    checkKeysHeld()
 
-    // TODO why doesn't this constrain work again?
-    // mouseX = constrain(mouseX, 0, width)
-    // mouseY = constrain(mouseY, 0, height)
+    // randomly assign indices for our pyramid emanating from the origin
+    // watch out! if you decrement the sphere detail in checkKeysHeld, there
+    // might be a concurrency error leading to an arrayOutOfBounds type error
+    if (frameCount % (144/2) === 0) {
+        projection_x = round(random(0, SPHERE_DETAIL-1))
+        projection_y = round(random(0, SPHERE_DETAIL-1))
+        console.log([projection_x, projection_y])
+    }
+}
 
 
+// add spherical coordinates to our globe array
+function populateGlobeArray() {
     /*
         according to wikipedia, spherical coordinates are done as (r, θ, φ)
         where θ is positive counterclockwise on the xy plane and φ is
@@ -103,28 +102,32 @@ function draw() {
             φ is clockwise on the zx/zy plane
      */
 
-    globe = Array(TOTAL+1)
-    for (let i=0; i<TOTAL+1; i++) {
-        globe[i] = Array(TOTAL+1)
+    /*  we need to add 1 to account for fence posts! if we want 8 sections,
+        i.e. a sphere detail level of 8, we have to end up were we started,
+        so we need 9 vertex "fence posts". otherwise, there will be a gap.
+
+        since sine wraps at 2π, the 9th vertex will always be equal to the
+        1st, i.e. the value at index 0 will equal the value at index 8 or TOTAL
+     */
+    globe = Array(SPHERE_DETAIL + 1)
+    for (let i = 0; i < globe.length; i++) {
+        globe[i] = Array(SPHERE_DETAIL + 1)
     }
 
     /*
         we want to convert (r, lat, lon) ➜ (x, y, z) in 3D; this is
         analogous to (r, θ) ➜ (r*cos(θ), r*sin(θ)) in 2D
 
-        θ is longitude,
-        φ is latitude,
+        θ is the polar angle, or angle on the x-y plane
+        φ is the zenith angle, or angle to the z-axis
         r is radial distance, commonly distance to origin
-
-        azimuthal angle. angle of rotation from meridian plane
-        polar angle. angle wrt polar axis, x
     */
     let θ, φ
-    let x, y, z, r=100
+    let x, y, z, r = 100
 
     // populate the globe 2D array
     // remember, angles start at 0 and are positive clockwise in p5!
-    for (let i=0; i<TOTAL+1; i++) {
+    for (let i = 0; i < globe.length; i++) {
         /*
             θ is the polar angle along x-y plane. LHR thumb points to z+
             θ is clockwise positive and starts at 1,0
@@ -134,8 +137,8 @@ function draw() {
             if we go for a full 2π radians, we get the entire xy plane circle
             this loop traverses quadrants 4, 3, 2, 1 in order on the xy plane
          */
-        θ = map(i, 0, TOTAL, 0, TAU)
-        for (let j=0; j<TOTAL+1; j++) {
+        θ = map(i, 0, SPHERE_DETAIL, 0, TAU)
+        for (let j = 0; j < globe[i].length; j++) {
             /*
                 φ is the angle from z+, positive clockwise
                 axis orientations in default easycam view:
@@ -144,7 +147,10 @@ function draw() {
                     z+ axis comes out of the page
              */
 
-            φ = map(j, 0, TOTAL, 0, PI) // this loop makes meridians
+            // should go from 0 to PI, but can go to TAU to generate extra
+            // set of points for wrapping. however this necessitates adding
+            // 2 at a time to the i length in globe[i][j], which maps θ
+            φ = map(j, 0, SPHERE_DETAIL, 0, PI) // this loop makes meridians
             // r*sin(φ) is a projection of r on the x-y plane
             x = r*sin(φ)*cos(θ)
             y = r*sin(φ)*sin(θ)
@@ -153,13 +159,18 @@ function draw() {
             globe[i][j] = new p5.Vector(x, y, z)
         }
     }
+}
 
+
+function displayGlobe() {
     strokeWeight(0.1)
     noFill()
-    // display globe using points or shapes!!
+    stroke(0, 0, 60, 20)
+
+    // display globe using vertices
     beginShape()
-    for (let i=0; i<TOTAL; i++)
-        for (let j=0; j<TOTAL; j++) {
+    for (let i = 0; i < globe.length-1; i++)
+        for (let j = 0; j < globe[i].length-1; j++) {
             let v1 = globe[i][j]
             let v2 = globe[i+1][j]
             let v3 = globe[i+1][j+1]
@@ -174,8 +185,13 @@ function draw() {
             vertex(v4.x, v4.y, v4.z)
         }
 
-    endShape()
+    endShape(CLOSE)
+}
 
+
+// draw our pyramid with rectangular base centered at the origin
+function drawPyramid() {
+    // this draws the rectangle pyramid
     // pyramid with lines https://editor.p5js.org/kchung/sketches/B17wokMUX
     beginShape(TRIANGLE_STRIP)
     strokeWeight(1)
@@ -208,15 +224,6 @@ function draw() {
         vertex(v.x, v.y, v.z)
     endShape()
 
-
-    displayHUD()
-    checkKeysHeld()
-
-    if (frameCount % 36 === 0) {
-        projection_x = round(random(0, TOTAL-1))
-        projection_y = round(random(0, TOTAL-1))
-        console.log([projection_x, projection_y])
-    }
 }
 
 
@@ -226,25 +233,25 @@ function checkKeysHeld() {
     if (keyIsDown(UP_ARROW) || keyIsDown(87)) { // w
         projection_y -= 1
         if (projection_y === -1)
-            projection_y = TOTAL - 1
+            projection_y = SPHERE_DETAIL - 1
     }
 
     if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) { // s
         projection_y += 1
-        if (projection_y === TOTAL)
+        if (projection_y === SPHERE_DETAIL)
             projection_y = 0
     }
 
     if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) { // a
         projection_x += 1
-        if (projection_x === TOTAL)
+        if (projection_x === SPHERE_DETAIL)
             projection_x = 0
     }
 
     if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) { // d
         projection_x -= 1
         if (projection_x === -1)
-            projection_x = TOTAL - 1
+            projection_x = SPHERE_DETAIL - 1
     }
 }
 
@@ -296,11 +303,24 @@ function drawBlenderAxes() {
 function keyPressed() {
     // let projection_x and projection_y values wrap around
     if (key === 'c') {
-        TOTAL += 1
+        SPHERE_DETAIL += 1
     }
 
     if (key === 'z') {
-        TOTAL -= 1
+        SPHERE_DETAIL -= 1
     }
+
+    // TODO try to make our arrow key methods functions so we can use both
+    //  isKeyDown as well as keyPressed to access them "once at a time" vs
+    //  "rapidfire"
+
+    /*  trying to fix the wrapping problem of φ only going from 0 to PI
+        but it turns out it's sort of okay to generate a duplicate set of points
+        in our globe so we have a second set of indices to access the same
+        vertices. wrapping is messy anyway because our up and down
+         directions would invert.
+       */
+
+
 }
 
